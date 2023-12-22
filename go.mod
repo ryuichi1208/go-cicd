ae9ca839d9b049aaae028ac37aa766bf5c21b48e
ryuichi1208 <ryucrosskey@gmail.com>
2023-12-22 23:30:38 +0900

    fix

--- go.mod
+++ go.mod
@@ -3,9 +3,29 @@ module github.com/ryuichi1208/go-cicd
 go 1.21.5
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible
+	go.opentelemetry.io/otel v1.21.0
+	go.opentelemetry.io/otel/sdk v1.21.0
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 var version string
@@ -30,4 +40,72 @@ func openAndRead() {
 		log.Fatal(err)
 	}
 	defer f.Close()
+}
+
+func httpServer() {
+	// インスタンスを作成
+	e := echo.New()
+
+	// ミドルウェアを設定
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	// サーバーをポート番号1323で起動
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func NewTracerProvider(serviceName string) (*sdktrace.TracerProvider, func(), error) {
+	exporter, err := NewJaegerExporter()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	r := NewResource(serviceName, "1.0.0", "local")
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(r),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+	)
+
+	otel.SetTracerProvider(tp)
+
+	cleanup := func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		if err := tp.ForceFlush(ctx); err != nil {
+			log.Print(err)
+		}
+		ctx2, cancel2 := context.WithTimeout(context.Background(), 5*time.Second)
+		if err := tp.Shutdown(ctx2); err != nil {
+			log.Print(err)
+		}
+		cancel()
+		cancel2()
+	}
+	return tp, cleanup, nil
+}
+
+func NewResource(serviceName string, version string, environment string) *resource.Resource {
+	return resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(serviceName),
+		semconv.ServiceVersionKey.String(version),
+		attribute.String("environment", environment),
+	)
+}
+
+func NewJaegerExporter() (sdktrace.SpanExporter, error) {
+	// Port details: https://www.jaegertracing.io/docs/getting-started/
+	endpoint := os.Getenv("EXPORTER_ENDPOINT")
+
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(endpoint)))
+	if err != nil {
+		return nil, err
+	}
+	return exporter, nil
 }
